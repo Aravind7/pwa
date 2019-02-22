81d0dea4efc175a6cf19531551897ae0bf5dd817
Aravind Manoharan <aravind.m@kgisl.com>
2019-02-21 22:04:01 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from  './api.service';
+import { Item } from  './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements  OnInit{
+ 
+  title = 'pwa';
+  items:  Array<Item>;
+
+  constructor(private  apiService:  ApiService){
+  }
+
+  ngOnInit(){
+    this.fetchData();
+    }
+
+    fetchData(){
+      this.apiService.fetch().subscribe((data:  Array<Item>)=>{
+      console.log(data);
+      this.items  =  data;
+      }, (err)=>{
+      console.log(err);
+      });
+      }
+}
